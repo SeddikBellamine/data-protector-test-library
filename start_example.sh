e617c5ef44d4c37b54267859bfe1e5da31570e70
SeddikBellamine <bellamine.seddik@gmail.com>
2023-03-21 01:05:16 +0100

    start example script

---
+++ start_example.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Clone the GitHub repo
+git clone https://github.com/SeddikBellamine/data-protector-test-library.git
+
+# Change to the project directory
+cd data-protector-test-library
+
+# Install dependencies using npm
+npm install
+
+# Start the development server using npm
+npm start
